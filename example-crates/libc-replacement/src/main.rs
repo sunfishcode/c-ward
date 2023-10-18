6039fd16e79aa7014bc465c26db443f514ee3e95
Dan Gohman <dev@sunfishcode.online>
2023-10-18 11:17:43 -0700

    Don't define `exit`, `atexit`, etc. in coexist-with-libc mode. (#40)
    When usingi libc, `origin::program::at_exit` is implemented in terms
    of `libc::atexit`, so don't have c-scape define `atexit` when in
    coexist-with-libc mode.

--- example-crates/libc-replacement/src/main.rs
+++ example-crates/libc-replacement/src/main.rs
@@ -1,4 +1,9 @@
 fn main() {
     println!("Hello, world! uid={}", unsafe { libc::getuid() });
     unsafe { libc::printf("Hello world with printf! gid=%u\n\0".as_ptr().cast(), libc::getgid()); }
+    unsafe { libc::atexit(atexit_func); }
+}
+
+extern "C" fn atexit_func() {
+    unsafe { libc::printf("Hello world from `atexit_func`\n\0".as_ptr().cast()); }
 }

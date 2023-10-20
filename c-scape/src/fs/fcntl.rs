731449ea15e48fc3757bf712b0442eddad5fe721
Dan Gohman <dev@sunfishcode.online>
2023-10-20 05:34:14 -0700

    Implement `fcntl64`.

--- c-scape/src/fs/fcntl.rs
+++ c-scape/src/fs/fcntl.rs
@@ -1,3 +1,4 @@
+use core::ffi::VaList;
 use rustix::fd::{BorrowedFd, IntoRawFd};
 use rustix::fs::{FdFlags, FlockOperation, OFlags};
 
@@ -7,6 +8,17 @@ use crate::convert_res;
 
 #[no_mangle]
 unsafe extern "C" fn fcntl(fd: c_int, cmd: c_int, mut args: ...) -> c_int {
+    let args = args.as_va_list();
+    _fcntl::<libc::flock>(fd, cmd, args)
+}
+
+#[no_mangle]
+unsafe extern "C" fn fcntl64(fd: c_int, cmd: c_int, mut args: ...) -> c_int {
+    let args = args.as_va_list();
+    _fcntl::<libc::flock64>(fd, cmd, args)
+}
+
+unsafe fn _fcntl<FlockTy: Flock>(fd: c_int, cmd: c_int, mut args: VaList) -> c_int {
     match cmd {
         libc::F_GETFL => {
             libc!(libc::fcntl(fd, libc::F_GETFL));
@@ -49,12 +61,12 @@ unsafe extern "C" fn fcntl(fd: c_int, cmd: c_int, mut args: ...) -> c_int {
             }
         }
         libc::F_SETLK | libc::F_SETLKW => {
-            let ptr = args.arg::<*mut libc::flock>();
+            let ptr = args.arg::<*mut FlockTy>();
             libc!(libc::fcntl(fd, cmd, ptr));
             let fd = BorrowedFd::borrow_raw(fd);
             let is_blocking = cmd == libc::F_SETLKW;
             let flock = &mut *ptr;
-            let op = match (flock.l_type as _, is_blocking) {
+            let op = match (flock.l_type() as _, is_blocking) {
                 (libc::F_RDLCK, true) => FlockOperation::LockShared,
                 (libc::F_WRLCK, true) => FlockOperation::LockExclusive,
                 (libc::F_UNLCK, true) => FlockOperation::Unlock,
@@ -64,12 +76,12 @@ unsafe extern "C" fn fcntl(fd: c_int, cmd: c_int, mut args: ...) -> c_int {
                 _ => unreachable!(),
             };
             // We currently only support whole-file locks.
-            assert_eq!(flock.l_whence, libc::SEEK_SET as _);
-            assert_eq!(flock.l_start, 0);
-            assert_eq!(flock.l_len, 0);
+            assert_eq!(flock.l_whence(), libc::SEEK_SET as _);
+            assert_eq!(flock.l_start(), 0);
+            assert_eq!(flock.l_len(), 0);
             match convert_res(rustix::fs::fcntl_lock(fd, op)) {
                 Some(()) => {
-                    flock.l_pid = -1;
+                    flock.l_pid(-1);
                     0
                 }
                 None => -1,
@@ -86,5 +98,49 @@ unsafe extern "C" fn fcntl(fd: c_int, cmd: c_int, mut args: ...) -> c_int {
             }
         }
         _ => panic!("unrecognized fnctl({})", cmd),
+    }
+}
+
+trait Flock {
+    fn l_type(&self) -> i16;
+    fn l_whence(&self) -> i16;
+    fn l_start(&self) -> libc::off64_t;
+    fn l_len(&self) -> libc::off64_t;
+    fn l_pid(&mut self, pid: libc::pid_t);
+}
+
+impl Flock for libc::flock {
+    fn l_type(&self) -> i16 {
+        self.l_type
+    }
+    fn l_whence(&self) -> i16 {
+        self.l_whence
+    }
+    fn l_start(&self) -> libc::off64_t {
+        self.l_start.into()
+    }
+    fn l_len(&self) -> libc::off64_t {
+        self.l_len.into()
+    }
+    fn l_pid(&mut self, pid: libc::pid_t) {
+        self.l_pid = pid;
+    }
+}
+
+impl Flock for libc::flock64 {
+    fn l_type(&self) -> i16 {
+        self.l_type
+    }
+    fn l_whence(&self) -> i16 {
+        self.l_whence
+    }
+    fn l_start(&self) -> libc::off64_t {
+        self.l_start
+    }
+    fn l_len(&self) -> libc::off64_t {
+        self.l_len
+    }
+    fn l_pid(&mut self, pid: libc::pid_t) {
+        self.l_pid = pid;
     }
 }

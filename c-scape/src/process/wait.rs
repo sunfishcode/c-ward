0a801a855f36f2f45efe9c8e4e91c37da85a4b4d
Dan Gohman <dev@sunfishcode.online>
2023-11-04 14:54:51 -0700

    Rustix now has `waitpgid`; use it.

--- c-scape/src/process/wait.rs
+++ c-scape/src/process/wait.rs
@@ -19,19 +19,6 @@ unsafe extern "C" fn waitpid(pid: c_int, status: *mut c_int, options: c_int) -> 
             Some(None) => return 0,
             None => return -1,
         },
-        // FIXME: This is a temporary workaround for rustix not supporting
-        // waitpid on process groups.
-        pid if pid < 0 => match convert_res(rustix::process::wait(options)) {
-            Some(Some((new_pid, new_status))) => {
-                ret_pid = new_pid.as_raw_nonzero().get() as c_int;
-                ret_status = new_status.as_raw() as c_int;
-            }
-            Some(None) => return 0,
-            None => return -1,
-        },
-        // FIXME: Use this version instead.
-        /*
-        // TODO: Give rustix better APIs for waiting on process groups.
         pid if pid < 0 => match convert_res(rustix::process::waitpgid(
             Pid::from_raw_unchecked(pid.wrapping_neg()),
             options,
@@ -47,7 +34,6 @@ unsafe extern "C" fn waitpid(pid: c_int, status: *mut c_int, options: c_int) -> 
             Some(None) => return 0,
             None => return -1,
         },
-        */
         pid => match convert_res(rustix::process::waitpid(Pid::from_raw(pid as _), options)) {
             Some(Some(new_status)) => {
                 ret_pid = if pid == 0 {

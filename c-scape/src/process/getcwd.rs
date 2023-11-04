e6000a03919536989039ce44d3585e636646f619
Dan Gohman <dev@sunfishcode.online>
2023-11-04 08:21:17 -0700

    Implement the glibc `getcwd(NULL, 0)` behavior.

--- c-scape/src/process/getcwd.rs
+++ c-scape/src/process/getcwd.rs
@@ -9,10 +9,25 @@ use crate::convert_res;
 unsafe extern "C" fn getcwd(buf: *mut c_char, len: usize) -> *mut c_char {
     libc!(libc::getcwd(buf, len));
 
+    if len == 0 && !buf.is_null() {
+        set_errno(Errno(libc::EINVAL));
+        return null_mut();
+    }
+
     match convert_res(rustix::process::getcwd(Vec::new())) {
         Some(path) => {
             let path = path.as_bytes();
+
+            let len = if len != 0 { len } else { path.len() + 1 };
+
             if path.len() + 1 <= len {
+                let mut buf = buf;
+                if buf.is_null() {
+                    buf = libc::malloc(len).cast::<c_char>();
+                    if buf.is_null() {
+                        return buf;
+                    }
+                }
                 copy_nonoverlapping(path.as_ptr().cast::<u8>(), buf.cast::<u8>(), path.len());
                 *buf.add(path.len()) = 0;
                 buf

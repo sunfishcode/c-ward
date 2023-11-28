12002d683b975aa9e14884c240155afcc3d799e9
Dan Gohman <dev@sunfishcode.online>
2023-11-27 23:31:12 -0800

    Implement `qsort_r`.

--- c-scape/src/sort/qsort.rs
+++ c-scape/src/sort/qsort.rs
@@ -41,6 +41,46 @@ unsafe extern "C" fn qsort(
     }
 }
 
+#[no_mangle]
+unsafe extern "C" fn qsort_r(
+    base: *mut c_void,
+    nmemb: size_t,
+    width: size_t,
+    compar: Option<unsafe extern "C" fn(*const c_void, *const c_void, *mut c_void) -> c_int>,
+    arg: *mut c_void,
+) {
+    libc!(libc::qsort_r(base, nmemb, width, compar, arg));
+
+    let compar = compar.unwrap();
+
+    if nmemb <= 1 {
+        return;
+    }
+
+    let base = base.cast::<u8>();
+    let mut gap = nmemb;
+
+    loop {
+        gap = next_gap(gap);
+
+        let mut any_swapped = false;
+        let mut a = base;
+        let mut b = base.add(gap * width);
+        for _ in 0..nmemb - gap {
+            if compar(a.cast(), b.cast(), arg) > 0 {
+                swap(a, b, width);
+                any_swapped = true;
+            }
+            a = a.add(width);
+            b = b.add(width);
+        }
+
+        if gap <= 1 && !any_swapped {
+            break;
+        }
+    }
+}
+
 fn next_gap(gap: size_t) -> size_t {
     let gap = (gap * 10) / 13;
 

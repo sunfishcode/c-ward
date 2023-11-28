2ee783913493ac2ab5f25369b490768ff652cae6
Dan Gohman <dev@sunfishcode.online>
2023-11-28 15:37:12 -0800

    Implement complex math support.
    Use the `num_complex` crate for the actual implementation.

--- c-scape/src/math/mod.rs
+++ c-scape/src/math/mod.rs
@@ -590,3 +590,14 @@ unsafe extern "C" fn nearbyint(x: f64) -> f64 {
 unsafe extern "C" fn nearbyintf(x: f32) -> f32 {
     libm::rintf(x)
 }
+
+// Enable support for complex numbers only on architectures where the builtin
+// C complex type has the same calling convention rules as a struct containing
+// two scalars. Notably, this excludes 32-bit "x86".
+#[cfg(any(
+    target_arch = "arm",
+    target_arch = "aarch64",
+    target_arch = "riscv64",
+    target_arch = "x86_64"
+))]
+mod complex;

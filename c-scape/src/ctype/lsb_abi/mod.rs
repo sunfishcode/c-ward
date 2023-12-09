9c63909dc26ad436feb2e3703ab49323fbd58a18
Dan Gohman <dev@sunfishcode.online>
2023-12-08 17:11:40 -0800

    Implement `__ctype_get_mb_cur_max`.

--- c-scape/src/ctype/lsb_abi/mod.rs
+++ c-scape/src/ctype/lsb_abi/mod.rs
@@ -2,8 +2,10 @@ mod ctype_b_loc;
 mod ctype_tolower_loc;
 mod ctype_toupper_loc;
 
-#[cfg(feature = "todo")]
 #[no_mangle]
 extern "C" fn __ctype_get_mb_cur_max() -> libc::size_t {
-    todo!("__ctype_get_mb_cur_max")
+    //libc!(libc::__ctype_get_mb_cur_max());
+
+    // Just C/POSIX.
+    1
 }

48189b2dcdd9d31dc5510d9b0815f257001a1a28
Kirpal Grewal <kirpal@outlook.com>
2023-12-29 16:37:22 +0000

    remove signnum from isinf

--- c-scape/src/math/mod.rs
+++ c-scape/src/math/mod.rs
@@ -749,12 +749,24 @@ unsafe extern "C" fn isnanf(x: f32) -> i32 {
 
 #[no_mangle]
 unsafe extern "C" fn isinf(x: f64) -> i32 {
-    return (x.is_infinite() as i32) * x.signum() as i32;
+    if x == f64::INFINITY {
+        return 1;
+    } else if x == f64::NEG_INFINITY {
+        return -1;
+    } else {
+        return 0;
+    }
 }
 
 #[no_mangle]
 unsafe extern "C" fn isinff(x: f32) -> i32 {
-    return (x.is_infinite() as i32) * x.signum() as i32;
+    if x == f32::INFINITY {
+        return 1;
+    } else if x == f32::NEG_INFINITY {
+        return -1;
+    } else {
+        return 0;
+    }
 }
 
 // Enable support for complex numbers only on architectures where the builtin

d809bd5f9b236bebc07f50caa3ee9086acbfbe48
Dan Gohman <dev@sunfishcode.online>
2023-12-29 17:07:26 -0800

    Add todo stubs for more long double functions.

--- c-scape/src/todo/long_double.rs
+++ c-scape/src/todo/long_double.rs
@@ -1,10 +1,286 @@
 //! Rust doesn't support ABI-compatible long double yet.
 
 #[no_mangle]
-unsafe extern "C" fn scalbnl() {
-    todo!("scalbnl")
+unsafe extern "C" fn nexttowardf() {
+    todo!("nexttowardf")
+}
+#[no_mangle]
+unsafe extern "C" fn nexttoward() {
+    todo!("nexttoward")
+}
+#[no_mangle]
+unsafe extern "C" fn nexttowardl() {
+    todo!("nexttowardl")
+}
+#[no_mangle]
+unsafe extern "C" fn acosl() {
+    todo!("acosl")
+}
+#[no_mangle]
+unsafe extern "C" fn acoshl() {
+    todo!("acoshl")
+}
+#[no_mangle]
+unsafe extern "C" fn asinl() {
+    todo!("asinl")
+}
+#[no_mangle]
+unsafe extern "C" fn asinhl() {
+    todo!("asinhl")
+}
+#[no_mangle]
+unsafe extern "C" fn atan2l() {
+    todo!("atan2l")
+}
+#[no_mangle]
+unsafe extern "C" fn atanl() {
+    todo!("atanl")
+}
+#[no_mangle]
+unsafe extern "C" fn atanhl() {
+    todo!("atanhl")
+}
+#[no_mangle]
+unsafe extern "C" fn cbrtl() {
+    todo!("cbrtl")
+}
+#[no_mangle]
+unsafe extern "C" fn ceill() {
+    todo!("ceill")
 }
 #[no_mangle]
 unsafe extern "C" fn copysignl() {
     todo!("copysignl")
+}
+#[no_mangle]
+unsafe extern "C" fn cosl() {
+    todo!("cosl")
+}
+#[no_mangle]
+unsafe extern "C" fn coshl() {
+    todo!("coshl")
+}
+#[no_mangle]
+unsafe extern "C" fn erfcl() {
+    todo!("erfcl")
+}
+#[no_mangle]
+unsafe extern "C" fn erfl() {
+    todo!("erfl")
+}
+#[no_mangle]
+unsafe extern "C" fn exp2l() {
+    todo!("exp2l")
+}
+#[no_mangle]
+unsafe extern "C" fn exp10l() {
+    todo!("exp10l")
+}
+#[no_mangle]
+unsafe extern "C" fn expl() {
+    todo!("expl")
+}
+#[no_mangle]
+unsafe extern "C" fn expm1l() {
+    todo!("expm1l")
+}
+#[no_mangle]
+unsafe extern "C" fn fabsl() {
+    todo!("fabsl")
+}
+#[no_mangle]
+unsafe extern "C" fn fdiml() {
+    todo!("fdiml")
+}
+#[no_mangle]
+unsafe extern "C" fn finitel() {
+    todo!("finitel")
+}
+#[no_mangle]
+unsafe extern "C" fn floorl() {
+    todo!("floorl")
+}
+#[no_mangle]
+unsafe extern "C" fn fmal() {
+    todo!("fmal")
+}
+#[no_mangle]
+unsafe extern "C" fn fmaxl() {
+    todo!("fmaxl")
+}
+#[no_mangle]
+unsafe extern "C" fn fminl() {
+    todo!("fminl")
+}
+#[no_mangle]
+unsafe extern "C" fn fmodl() {
+    todo!("fmodl")
+}
+#[no_mangle]
+unsafe extern "C" fn frexpl() {
+    todo!("frexpl")
+}
+#[no_mangle]
+unsafe extern "C" fn hypotl() {
+    todo!("hypotl")
+}
+#[no_mangle]
+unsafe extern "C" fn ilogbl() {
+    todo!("ilogbl")
+}
+#[no_mangle]
+unsafe extern "C" fn isinfl() {
+    todo!("isinfl")
+}
+#[no_mangle]
+unsafe extern "C" fn isnanl() {
+    todo!("isnanl")
+}
+#[no_mangle]
+unsafe extern "C" fn j0l() {
+    todo!("j0l")
+}
+#[no_mangle]
+unsafe extern "C" fn j1l() {
+    todo!("j1l")
+}
+#[no_mangle]
+unsafe extern "C" fn jnl() {
+    todo!("jnl")
+}
+#[no_mangle]
+unsafe extern "C" fn ldexpl() {
+    todo!("ldexpl")
+}
+#[no_mangle]
+unsafe extern "C" fn lgammal() {
+    todo!("lgammal")
+}
+#[no_mangle]
+unsafe extern "C" fn lgammal_r() {
+    todo!("lgammal_r")
+}
+#[no_mangle]
+unsafe extern "C" fn log1pl() {
+    todo!("log1pl")
+}
+#[no_mangle]
+unsafe extern "C" fn log2l() {
+    todo!("log2l")
+}
+#[no_mangle]
+unsafe extern "C" fn log10l() {
+    todo!("log10l")
+}
+#[no_mangle]
+unsafe extern "C" fn logl() {
+    todo!("logl")
+}
+#[no_mangle]
+unsafe extern "C" fn modfl() {
+    todo!("modfl")
+}
+#[no_mangle]
+unsafe extern "C" fn nextafterl() {
+    todo!("nextafterl")
+}
+#[no_mangle]
+unsafe extern "C" fn powl() {
+    todo!("powl")
+}
+#[no_mangle]
+unsafe extern "C" fn remainderl() {
+    todo!("remainderl")
+}
+#[no_mangle]
+unsafe extern "C" fn dreml() {
+    todo!("dreml")
+}
+#[no_mangle]
+unsafe extern "C" fn remquol() {
+    todo!("remquol")
+}
+#[no_mangle]
+unsafe extern "C" fn roundl() {
+    todo!("roundl")
+}
+#[no_mangle]
+unsafe extern "C" fn scalbnl() {
+    todo!("scalbnl")
+}
+#[no_mangle]
+unsafe extern "C" fn scalblnl() {
+    todo!("scalblnl")
+}
+#[no_mangle]
+unsafe extern "C" fn scalbl() {
+    todo!("scalbl")
+}
+#[no_mangle]
+unsafe extern "C" fn sincosl() {
+    todo!("sincosl")
+}
+#[no_mangle]
+unsafe extern "C" fn sinl() {
+    todo!("sinl")
+}
+#[no_mangle]
+unsafe extern "C" fn sinhl() {
+    todo!("sinhl")
+}
+#[no_mangle]
+unsafe extern "C" fn sqrtl() {
+    todo!("sqrtl")
+}
+#[no_mangle]
+unsafe extern "C" fn tanl() {
+    todo!("tanl")
+}
+#[no_mangle]
+unsafe extern "C" fn tanhl() {
+    todo!("tanhl")
+}
+#[no_mangle]
+unsafe extern "C" fn tgammal() {
+    todo!("tgammal")
+}
+#[no_mangle]
+unsafe extern "C" fn truncl() {
+    todo!("truncl")
+}
+#[no_mangle]
+unsafe extern "C" fn y0l() {
+    todo!("y0l")
+}
+#[no_mangle]
+unsafe extern "C" fn y1l() {
+    todo!("y1l")
+}
+#[no_mangle]
+unsafe extern "C" fn ynl() {
+    todo!("ynl")
+}
+#[no_mangle]
+unsafe extern "C" fn rintl() {
+    todo!("rintl")
+}
+#[no_mangle]
+unsafe extern "C" fn logbl() {
+    todo!("logbl")
+}
+#[no_mangle]
+unsafe extern "C" fn lrintl() {
+    todo!("lrintl")
+}
+#[no_mangle]
+unsafe extern "C" fn llrintl() {
+    todo!("llrintl")
+}
+#[no_mangle]
+unsafe extern "C" fn lroundl() {
+    todo!("lroundl")
+}
+#[no_mangle]
+unsafe extern "C" fn llroundl() {
+    todo!("llroundl")
 }

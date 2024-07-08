841c564afb8b016058a7860b00cef157785b9f43
Dan Gohman <dev@sunfishcode.online>
2024-07-08 07:30:53 -0700

    Update to origin 0.19.0.

--- c-scape/src/lib.rs
+++ c-scape/src/lib.rs
@@ -165,9 +165,11 @@ fn convert_res<T>(result: Result<T, rustix::io::Errno>) -> Option<T> {
 /// A type that implements `lock_api::GetThreadId` for use with
 /// `lock_api::RawReentrantMutex`.
 #[cfg(feature = "thread")]
+#[cfg(feature = "take-charge")]
 pub(crate) struct GetThreadId;
 
 #[cfg(feature = "thread")]
+#[cfg(feature = "take-charge")]
 unsafe impl rustix_futex_sync::lock_api::GetThreadId for GetThreadId {
     const INIT: Self = Self;
 
